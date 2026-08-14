e8da151bcbf75bebe2e5c7762ba125186a0ef232
aliffahrizi <aliffahrizi@gmail.com>
2026-08-14 18:28:46 +0700

    prototype 1 of cursor moving with the audio

--- src/main.py
+++ src/main.py
@@ -3,73 +3,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
 from scipy.signal import stft, spectrogram
+from matplotlib.animation import FuncAnimation
+import time
 
-song_name = 'Daydreaming'
+song_name = 'Intro'
 
-audio, sampleRate = sf.read(r"audio\Radiohead - Daydreaming.flac")
+audio, sampleRate = sf.read(r"audio\The xx - Intro.flac")
 print(f'Audio shape: {audio.shape}')
 print(f'Sample rate: {sampleRate}')
 
-time = np.arange(len(audio)) / sampleRate
+t = np.arange(len(audio)) / sampleRate
 
-# plt.plot(time, audio[:, 0])
-# plt.xlabel("Time")
-# plt.ylabel("Amplitude")
-# plt.show()
-
-
+# duration = np.max(t) / 5
 duration = 10
 n_samples = int(duration * sampleRate)
 
+# audio separation for 10 secs
 x = audio[:n_samples, :]
 x_left= audio[:n_samples, 0]
 x_right = audio[:n_samples, 1]  
+    
+    
+f, t, Zxx = stft(x_right, fs=sampleRate, nperseg=2048, noverlap=1024
+)
 
-# X = np.fft.rfft(x)
-# freq = np.fft.rfftfreq(len(x), 1 / sampleRate)
-
-# magnitude = np.abs(X)
-
-# plt.plot(freq, magnitude)
-
-# plt.xlabel("Frequency (Hz)")
-# plt.ylabel("Magnitude")
-# plt.title("FFT of Daydreaming")
-# plt.xscale("log")
-# plt.xlim(20, 20000)
-
-# plt.show()
-
-
-sd.play(x)
-# sd.wait()
-
-
-
-
-freq, time, Zxx = stft(x_right, fs=sampleRate, nperseg=2048, noverlap=1024)
-
-# magnitude = np.abs(Zxx)
 magnitude_db = 20 * np.log10(np.abs(Zxx) + 1e-10)
 
-plt.pcolormesh(time, freq, magnitude_db, shading='gouraud', cmap='inferno', vmin=-120, vmax=0)
-plt.colorbar(label='Magnitude (dB)')
-plt.xlabel('Time (s)')
-plt.yscale('log')
-plt.ylim(20, 20000)
-plt.ylabel('Frequency (Hz)')
-plt.title(song_name)
-plt.show()
-# freq, time, Sxx = stft(x, fs=sampleRate, nperseg=2048, noverlap=1024)
+fig, ax = plt.subplots()
 
-# # magnitude = np.abs(Zxx)
-# magnitude_db = 20 * np.log10(np.abs(Zxx) + 1e-10)
+mesh = ax.pcolormesh(
+    t,
+    f,
+    magnitude_db,
+    shading="gouraud",
+    cmap="inferno",
+    vmin=-120,
+    vmax=0
+)
+cursor = ax.axvline(0)
+ax.set_xlabel("Time (s)")
+ax.set_ylabel("Frequency (Hz)")
+ax.set_yscale("log")
+ax.set_ylim(20, 20000)
+ax.set_title(song_name)
 
-# plt.pcolormesh(time, freq, magnitude_db, shading='gouraud', cmap='inferno', vmin=-160, vmax=0)
-# plt.colorbar(label='Magnitude (dB)')
-# plt.xlabel('Time (s)')
-# plt.yscale('log')
-# plt.ylim(20, 20000)
-# plt.ylabel('Frequency (Hz)')
-# plt.title(song_name)
-# plt.show()
+
+
+sd.play(audio[:n_samples, :], sampleRate)
+start_time = time.perf_counter()
+fig.colorbar(mesh, ax=ax, label="Magnitude (dB)")
+plt.show(block=False)
+while sd.get_stream().active:
+    elapsed = time.perf_counter() - start_time
+    cursor.set_xdata([elapsed, elapsed])
+    plt.pause(0.01)
+
+sd.wait()
+plt.close()
