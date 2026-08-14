3fb62775ccef684aaa2fad41a64884edb72ae41f
aliffahrizi <aliffahrizi@gmail.com>
2026-08-14 20:56:50 +0700

    cleaned main, jitter still there, add mouse function

--- src/main.py
+++ src/main.py
@@ -2,29 +2,29 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
-from scipy.signal import stft, spectrogram
-from matplotlib.animation import FuncAnimation
+from scipy.signal import stft
 import time
 
 song_name = 'Intro'
 
 audio, sampleRate = sf.read(r"audio\The xx - Intro.flac")
+
 print(f'Audio shape: {audio.shape}')
 print(f'Sample rate: {sampleRate}')
 
-t = np.arange(len(audio)) / sampleRate
-
-# duration = np.max(t) / 5
 duration = 10
 n_samples = int(duration * sampleRate)
 
-# audio separation for 10 secs
+# Audio separation for 10 secs
 x = audio[:n_samples, :]
-x_left= audio[:n_samples, 0]
-x_right = audio[:n_samples, 1]  
-    
-    
-f, t, Zxx = stft(x_right, fs=sampleRate, nperseg=2048, noverlap=1024
+x_left = audio[:n_samples, 0]
+x_right = audio[:n_samples, 1]
+
+f, t, Zxx = stft(
+    x_right,
+    fs=sampleRate,
+    nperseg=2048,
+    noverlap=1024
 )
 
 magnitude_db = 20 * np.log10(np.abs(Zxx) + 1e-10)
@@ -40,22 +40,44 @@ mesh = ax.pcolormesh(
     vmin=-120,
     vmax=0
 )
+
 cursor = ax.axvline(0)
+
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Frequency (Hz)")
 ax.set_yscale("log")
 ax.set_ylim(20, 20000)
 ax.set_title(song_name)
 
+fig.colorbar(mesh, ax=ax, label="Magnitude (dB)")
 
+
+def mouse_move(event):
+    if event.inaxes == ax:
+        print(
+            f"Time: {event.xdata:.2f} s | "
+            f"Frequency: {event.ydata:.1f} Hz"
+        )
+
+
+fig.canvas.mpl_connect(
+    "motion_notify_event",
+    mouse_move
+)
+
+plt.show(block=False)
+
+# Give Matplotlib time to render
+plt.pause(1)
 
 sd.play(audio[:n_samples, :], sampleRate)
 start_time = time.perf_counter()
-fig.colorbar(mesh, ax=ax, label="Magnitude (dB)")
-plt.show(block=False)
+
 while sd.get_stream().active:
     elapsed = time.perf_counter() - start_time
+
     cursor.set_xdata([elapsed, elapsed])
+
     plt.pause(0.01)
 
 sd.wait()
