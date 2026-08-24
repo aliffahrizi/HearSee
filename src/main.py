c592a5725220822ee56fb96d359519ec0416be61
aliffahrizi <aliffahrizi@gmail.com>
2026-08-24 14:55:07 +0700

    added  mouse movement analysis

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ audio, sampleRate = sf.read(audio_path)
 print(f'Audio shape: {audio.shape}')
 print(f'Sample rate: {sampleRate}')
 
-duration = 10
+duration = 5
 n_samples = int(duration * sampleRate)
 
 # Audio separation for 10 secs
@@ -55,13 +55,31 @@ ax.set_title(song_name)
 
 fig.colorbar(mesh, ax=ax, label="Magnitude (dB)")
 
+fig.subplots_adjust(bottom=0.22)
+info_text = ax.text(
+    0.02, -0.15,
+    "",
+    transform=ax.transAxes,
+    verticalalignment="top"
+)
 
 def mouse_move(event):
     if event.inaxes == ax:
-        print(
-            f"Time: {event.xdata:.2f} s | "
-            f"Frequency: {event.ydata:.1f} Hz"
+        
+        mouse_time = event.xdata
+        mouse_freq = event.ydata
+
+        time_index = np.argmin(np.abs(t - mouse_time))
+        freq_index = np.argmin(np.abs(f - mouse_freq))
+
+        magnitude = magnitude_db[freq_index, time_index]
+
+        info_text.set_text(
+            f"Time: {mouse_time:.2f} s\n"
+            f"Frequency: {mouse_freq:.1f} Hz\n"
+            f"Magnitude: {magnitude:.1f} dB"
         )
+        fig.canvas.draw_idle()
 
 
 fig.canvas.mpl_connect(
@@ -69,20 +87,20 @@ fig.canvas.mpl_connect(
     mouse_move
 )
 
-plt.show(block=False)
+plt.show()
+# plt.show(block=False)
 
-# Give Matplotlib time to render
-plt.pause(1)
+# plt.pause(10)
 
-sd.play(audio[:n_samples, :], sampleRate)
-start_time = time.perf_counter()
+# sd.play(audio[:n_samples, :], sampleRate)
+# start_time = time.perf_counter()
 
-while sd.get_stream().active:
-    elapsed = time.perf_counter() - start_time
+# while sd.get_stream().active:
+#     elapsed = time.perf_counter() - start_time
 
-    cursor.set_xdata([elapsed, elapsed])
+#     cursor.set_xdata([elapsed, elapsed])
 
-    plt.pause(0.01)
+#     plt.pause(0.01)
 
-sd.wait()
-plt.close()
+# sd.wait()
+# plt.close()
